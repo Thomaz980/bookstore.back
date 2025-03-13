fb52d4b5173146a27ef5846ed4b9eca3aadbf90c
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2025-03-12 23:51:52 -0300

    feat(model): adicionar models Book e User

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? Author { get; set; }
+		public string? Genre { get; set; }
+		public string? Category { get; set; }
+		public string? Language { get; set; }
+		public int TotalPages { get; set; }
+		public string? CoverImageUrl { get; set; }
+		public string? BookPdfUrl { get; set; }
+		public int UserId { get; set; }
+
+		[ForeignKey("UserId")]
+		public User? User { get; set; }
+	}
+}
